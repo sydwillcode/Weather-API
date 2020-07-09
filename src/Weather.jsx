15d5646b9ed244c85cc3c78193ad6586d026fdd9
Sydni <will.sydni@gmail.com>
2020-07-09 08:52:04 -0700

    Minor Style Changes

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,15 +1,12 @@
 import React, { useState, useEffect, Fragment } from "react";
+import "./Weather.css";
 
 let city = "Chicago";
-
 //create functional component
 const Weather = () => {
   const [weather, setWeather] = useState([]);
-
   const getWeather = async () => {
-    /*get request requires city and API Key. 
-        Set variable for current API Key */
-
+    //Protect API Key 
     const appid = process.env.REACT_APP_WEATHER_API_KEY;
     let res = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&lang=en&appid=${appid}`
@@ -28,26 +25,32 @@ const Weather = () => {
 
   return (
     <>
-      <div className="container">
+      <div className="container ">
         <div className="weather main-info">
           <>
-            <div>{city}</div>
+            <div className="city">{city.toUpperCase()}</div>
             {weather.weather && (
-              <img
-                src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-                alt="Weather Icon"
-              />
+              <>
+                <div>{weather.weather[0].description.toUpperCase()}</div>
+                <img
+                  src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                  alt="Weather Icon"
+                />
+              </>
             )}
-            {weather.weather && <div>{weather.weather[0].description}</div>}
-            {weather.main && <h3>{weather.main.temp}</h3>}
+            {weather.main && <div className="current-temp">{weather.main.temp} F</div>}
           </>
         </div>
 
         <div className="weather secondary-info">
           <>
-            {weather.main && <div>Feels Like: {weather.main.feels_like}</div>}
-            {weather.main && <div>Humidity: {weather.main.humidity}</div>}
-            {weather.wind && <div>Wind Speed: {weather.wind.speed}</div>}
+            {weather.main && (
+              <>
+                <div>Feels Like: {weather.main.feels_like} F</div>
+                <div>Humidity: {weather.main.humidity} %</div>
+              </>
+            )}
+            {weather.wind && <div>Wind Speed: {weather.wind.speed} MPH</div>}
           </>
         </div>
       </div>
